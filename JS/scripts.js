ddc55dcf17e97e12a304165dea4d0b6d763453da
khilbert47 <78921470+khilbert47@users.noreply.github.com>
2021-03-03 16:56:39 -0700

    Created Menu Toggle

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,12 @@
 console.log("Welcome to My Portfolio! Enjoy!")
 
+// Menu toggle for mobile to reduce menu size on mobile only
+function menuToggle() {
+	var x = document.getElementById('myNavtoggle');
+	if (x.className === 'navtoggle') {
+		x.className += ' responsive';
+	} else {
+		x.className = 'navtoggle';
+	}
+	
+}
