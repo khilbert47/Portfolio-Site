c113e1b8923a02c68b25320258d38406aa68c380
khilbert47 <78921470+khilbert47@users.noreply.github.com>
2021-03-03 16:58:02 -0700

    Added Smooth Scrolling

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,3 +10,14 @@ function menuToggle() {
 	}
 	
 }
+
+$(document).ready(function() {
+	var scrollLink = $('.scroll')
+	// Smooth scrolling for better user experience
+	scrollLink.click(function(event) {
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1500 );
+	} )
+} )
