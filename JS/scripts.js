89669a17dff4754f8ac36f26bce83b15b8cfec56
khilbert47 <78921470+khilbert47@users.noreply.github.com>
2022-05-25 09:22:14 -0500

    Added Smooth Scrolling

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,22 @@
-console.log("Welcome to My Portfolio! Enjoy!")
+console.log('Welcome to My Portfolio! Enjoy!')
 
-// Menu toggle for mobile to reduce menu size on mobile only
-function menuToggle() {
-	var x = document.getElementById('myNavtoggle');
-	if (x.className === 'navtoggle') {
-		x.className += ' responsive';
-	} else {
-		x.className = 'navtoggle';
-	}
-
+// Menu toggle for mobile to reduce menu size, mobile only
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
 
-$(document).ready(function() {
-	var scrollLink = $('.scroll')
-	// Smooth scrolling for better user experience
-	scrollLink.click(function(event) {
-		event.preventDefault();
-		$('body,html').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 1500 );
-	} )
-} )
+$(document).ready(function () {
+  var scrollLink = $('.scroll')
+  // Smooth scrolling for better user experience
+  scrollLink.click(function (event) {
+    event.preventDefault()
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1500)
+  })
+})
